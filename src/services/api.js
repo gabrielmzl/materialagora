df4c89b7cdca464ab7327ab8a1c4026985debe94
gabrielmzl <61896505+gabrielmzl@users.noreply.github.com>
2022-10-29 01:12:45 -0300

    fix: url api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://superheroapi.com/api/1459450631132108',
+  baseURL: 'https://superheroapi.com/api.php/1459450631132108',
 });
 
 export default api;
